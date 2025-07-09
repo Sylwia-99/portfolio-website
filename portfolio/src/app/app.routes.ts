5f585d74870077fe121ffed98685b6eaaefb8170
Sylwia Mosur <sylwia02.02.1999@interia.pl>
2025-07-09 22:58:00 +0200

    feat: an about me page was added and it was styled

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AboutMeComponent }];
