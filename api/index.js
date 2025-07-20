4a4ef8da8ddb632b5adc2c3eb0607d42d5a1ce0e
Sylwia Mosur <sylwia02.02.1999@interia.pl>
2025-07-20 13:32:01 +0200

    fix: configurations have been fixed

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const { app } = require("../dist/browser/server/main");
-const server = require("http").createServer(app);
-
-module.exports = (req, res) => {
-  server.emit("request", req, res);
-};
