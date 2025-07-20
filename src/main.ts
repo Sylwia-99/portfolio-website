8cfdeda7a2d1d621d5e7303aeef6b4975ee85e24
Sylwia Mosur <sylwia02.02.1999@interia.pl>
2025-07-20 20:26:13 +0200

    fix: vercel's configurations have been fixed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)],
